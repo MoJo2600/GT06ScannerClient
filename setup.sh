f220f700f6d240126ce467dba1f2a09f44dc8cec
Christian Erhardt <christian.erhardt@mojo2k.de>
2023-03-29 20:14:51 +0200

    fix: files

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@ set -e
 set -x
 
 rm -rf venv/
+sudo cp systemd/gpstracker.service /etc/systemd/system
 sudo mkdir -p /opt/gpstracker
 sudo cp -Rfv gpstracker/ /opt/
 cd /opt/gpstracker
 sudo python3 -m venv venv
 sudo ./venv/bin/pip3 install -r requirements.txt
-sudo cp systemd/gpstracker.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable gpstracker
 sudo systemctl start gpstracker
